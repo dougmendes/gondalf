4c466ee77421f0c35b572479d73f044fa5404522
Douglas Mendes <douglasleomendes@hotmail.com>
2022-11-03 19:21:26 -0300

    fix initial files

--- pkg/authentication/controller/register.go
+++ pkg/authentication/controller/register.go
@@ -1,7 +1,20 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Register(c *gin.Context) {
+	c.JSON(http.StatusNotImplemented, gin.H{
+		"message": "not implement yet",
+	})
+
+}
+func Pong(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong!!",
+	})
 
 }
