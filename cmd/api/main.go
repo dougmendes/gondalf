87ea7af048af5cb954f176c52c94a041413e151f
Douglas Mendes <douglasleomendes@hotmail.com>
2022-11-11 15:49:00 -0300

    feat: add folder structure for project generator

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	instances "github.com/dougmendes/gondalf/pkg/instances/controller"
+	projectGenerator "github.com/dougmendes/gondalf/pkg/project-generator/controller"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", instances.Pong)
+	r.POST("/project-generator", projectGenerator.NewProject)
 	r.Run()
 }
