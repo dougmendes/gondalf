52a29883545b3d9d34c792e0a5ab7393d611330f
Douglas Mendes <douglasleomendes@hotmail.com>
2022-11-11 08:01:19 -0300

    fix: refactoring folders and files

--- pkg/instances/controller/tests/instance_test.go
+++ pkg/instances/controller/tests/instance_test.go
@@ -4,27 +4,28 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"testing"
 
 	. "github.com/dougmendes/gondalf/pkg/instances/controller"
 	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-func SetUpRouter() *gin.Engine {
-	router := gin.Default()
-	return router
-}
-func TestInstance(t *testing.T) {
-	mockResponse := `{"message":"pong!!"}`
-	r := SetUpRouter()
-	r.GET("/ping", Pong)
-	req, _ := http.NewRequest("GET", "/ping", nil)
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
+var _ = Describe("Ping", func() {
+	When("the user send a ping", func() {
+		r := gin.Default()
+		w := httptest.NewRecorder()
+		BeforeEach(func() {
+			r.GET("/ping", Pong)
+			req, _ := http.NewRequest("GET", "/ping", nil)
+			r.ServeHTTP(w, req)
+		})
+		It("respond with pong", func() {
 
-	responseData, _ := ioutil.ReadAll(w.Body)
-	assert.Equal(t, mockResponse, string(responseData))
-	assert.Equal(t, http.StatusOK, w.Code)
-
-}
+			mockResponse := `{"message":"pong!!"}`
+			responseData, _ := ioutil.ReadAll(w.Body)
+			Expect(string(responseData)).To(Equal(mockResponse))
+			Expect(w.Code).To(Equal(200))
+		})
+	})
+})
